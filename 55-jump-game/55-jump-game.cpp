4dd22225e596e5c31e9a84b45cd80f83e6852e83
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-01-15 10:38:25 +0530

    Time: 56 ms (84.04%), Space: 48.3 MB (49.17%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxreach = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(i > maxreach) return false;
+            
+            maxreach = max(maxreach, i + nums[i]);
+        }
+        return true;
+    }
+};
