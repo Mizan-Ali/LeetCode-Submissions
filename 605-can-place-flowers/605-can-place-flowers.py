df6117b7f40a34dbe06950e8441ce80e58b22770
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-01-18 10:29:02 +0530

    Time: 270 ms (15.77%), Space: 14.5 MB (84.06%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -10,18 +10,16 @@ class Solution:
                 return False
             
         if flowerbed[0] == 0 and flowerbed[1] == 0:
-            print('isdf')
             flowerbed[0] = 1
-            print(flowerbed)
             count += 1
-        print(flowerbed)
+        
         for i in range(1, len(flowerbed)-1):
-            print('isme?')
             if flowerbed[i] == 0:
                 if flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
                     flowerbed[i] = 1
-                    print('isme bhi jarha')
                     count += 1
+                if count == n:
+                    return True
         if flowerbed[-1] == 0 and flowerbed[-2] == 0:
             count += 1
             flowerbed[-1] = 1
