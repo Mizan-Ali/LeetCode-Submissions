fd9e45a3103dc1c28cdc63e8f926b3c99bd67079
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-01-21 10:18:23 +0530

    Time: 3 ms (46.62%), Space: 63.8 MB (39.94%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,13 +1,19 @@
-class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(cost) > sum(gas):
-            return -1
-        total = 0
-        res = 0
-        for i in range(len(gas)):
-            total += (gas[i] - cost[i])
-            
-            if total < 0:
-                total = 0
-                res = i + 1
-        return res
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sumG = 0, sumC = 0;
+        for(int i = 0; i < gas.length; i++) {
+            sumG += gas[i];
+            sumC += cost[i];
+        }
+        if(sumG < sumC) return -1;
+        int total = 0, res = 0;
+        for(int i=0; i<gas.length; i++) {
+            total += gas[i] - cost[i];
+            if(total < 0) {
+                total = 0;
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+}
