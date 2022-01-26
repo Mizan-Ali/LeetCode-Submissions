1dbca67c819db087eda2d2b820c25d35ec01780d
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-01-26 10:41:54 +0530

    Time: 431 ms (50.83%), Space: 21.2 MB (66.26%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,19 +7,31 @@
 
 # FIRST APPROACH
 class Solution:
-    def traverse(self, node):
-        global trav
+    def traverse(self, node, trav):
         if node == None:
             return
-        self.traverse(node.left)
+        self.traverse(node.left, trav)
         trav.append(node.val)
-        self.traverse(node.right)
+        self.traverse(node.right, trav)
         
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        global trav
-        trav = []
-        self.traverse(root1)
-        print(trav)
-        self.traverse(root2)
-        # trav = sorted(trav, key=lambda x: x.val)
-        return sorted(trav)
+        trav1, trav2 = [], []
+        i, j = 0, 0
+        self.traverse(root1, trav1)
+        self.traverse(root2, trav2)
+        ans = []
+        while i < len(trav1) and j < len(trav2):
+            if trav1[i] <= trav2[j]:
+                ans.append(trav1[i])
+                i += 1
+            else:
+                ans.append(trav2[j])
+                j += 1
+        while i < len(trav1):
+            ans.append(trav1[i])
+            i += 1
+        while j < len(trav2):
+            ans.append(trav2[j])
+            j += 1
+        return ans
+        
