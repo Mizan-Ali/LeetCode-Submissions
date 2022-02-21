fb2ae1d30a2781df1a204bd23ad94d4d305de05a
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-02-21 11:08:57 +0530

    Time: 13 ms (27.15%), Space: 56.4 MB (13.74%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,23 @@
+import java.util.Map.Entry;
+class Solution {
+    public int majorityElement(int[] nums) {
+        float n = nums.length;
+        Map<Integer, Integer> count = new HashMap<>();
+        
+        for(int val: nums) {
+            if(count.containsKey(val)) {
+                count.put(val, count.get(val)+1);
+            }
+            else {
+                count.put(val, 1);
+            }
+        }
+        for(Entry<Integer, Integer> keyval: count.entrySet()) {
+            // System.out.println(keyval.getKey() + " : " + keyval.getValue());
+            if(keyval.getValue() >= n/2) {
+                return keyval.getKey();
+            }
+        }
+        return 0;
+    }
+}
