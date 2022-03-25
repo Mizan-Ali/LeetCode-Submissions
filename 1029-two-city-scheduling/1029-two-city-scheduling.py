a992febdae6d78058fa5a05b768171a52e715af0
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-03-25 09:17:11 +0530

    Time: 48 ms (73.52%), Space: 13.9 MB (45.30%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -2,17 +2,13 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         hmap = [[(a-b),[a,b]] for a, b in costs]
         hmap = sorted(hmap, key= lambda x: x[0], reverse=True)
-        print(hmap)
         ans = 0
         n = 1
-    
         for val in hmap:
             if n > len(costs)//2:
                 ans += val[1][0]
-                print(val[1][0])
             else:
                 ans += val[1][1]
-                print(val[1][1])
             n += 1
         return ans
         
