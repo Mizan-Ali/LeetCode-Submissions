7b3cc6df6bcce7e74c6c5456eb4dc5a65c77c878
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-01 10:49:07 +0530

    Time: 171 ms (5.21%), Space: 14.1 MB (40.05%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,34 +1,20 @@
 class Solution:
-    def isValid(self, visited, image, color, i, j):
-        row, col = len(image), len(image[0])
-        if 0 <= i < row and 0 <= j < col:
-            if(image[i][j] == color and (i, j) not in visited):
-                return True
-        return False
-    def getNeighbors(self, visited, image, i, j):
-        neighbors = []
-        row, col = len(image), len(image[0])
-        color = image[i][j]
-        if self.isValid(visited, image, color, i+1, j):
-            neighbors.append([i+1, j])
-        if self.isValid(visited, image, color, i-1, j):
-            neighbors.append([i-1, j])
-        if self.isValid(visited, image, color, i, j+1):
-            neighbors.append([i, j+1])
-        if self.isValid(visited, image, color, i, j-1):
-            neighbors.append([i, j-1])
-        return neighbors
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        # Breadth first search
-        queue = [[sr, sc]]
-        visited = {}
-        while queue:
-            i, j = queue.pop(0)
-            visited[(i, j)] = True
-            neighbors = self.getNeighbors(visited, image, i, j)
-            image[i][j] = newColor
-            queue.extend(neighbors)
-            # print(queue)
+        row, col = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor:
+            return image
+        def dfs(i, j):
+            if image[i][j] == color:
+                image[i][j] = newColor
+                
+                if i >= 1:
+                    dfs(i-1, j)
+                if i+1 < row:
+                    dfs(i+1, j)
+                if j >= 1:
+                    dfs(i, j-1)
+                if j+1 < col:
+                    dfs(i, j+1)
+        dfs(sr, sc)
         return image
-            
-            
