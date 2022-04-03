bbf43b4667200548c497ca97f357692be52ecce5
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-03 11:30:41 +0530

    Time: 616 ms (93.04%), Space: 14.4 MB (59.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,36 +1,25 @@
 class Solution:
-    def isValid(self, i, j, grid):
-        row, col = len(grid), len(grid[0])
-        if 0<=i<row and 0<=j<col and grid[i][j] == 0:
-            return True
-        return False
-    
-    def getNeighbors(self, i, j, visited, grid):
-        x = [-1, -1, -1, 0, +1, +1, +1, 0]
-        y = [-1, 0, +1, +1, +1, 0, -1, -1]
-        neighbors = []
-        for a, b in zip(x, y):
-            if self.isValid(i+a, j+b, grid):
-                if (i+a, j+b) not in visited:
-                    neighbors.append((i+a, j+b))
-        return neighbors
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] != 0:
             return -1
+        # For 8 directional movement
+        x = [-1, -1, -1, 0, +1, +1, +1, 0]
+        y = [-1, 0, +1, +1, +1, 0, -1, -1]
+        
         # apply bfs
         n = len(grid)
-        queue = [(0, 0)]
-        dist = 0
-        visited = {(0,0): True}
+        queue = [(0, 0, 1)]
+        
         while queue:
-            size = len(queue)
-            dist += 1
-            for _ in range(size):
-                i, j = queue.pop(0)
-                if i == n-1 and j == n-1:
-                    return dist
-                neighbors = self.getNeighbors(i, j, visited, grid)
-                for a, b in neighbors:
-                    queue.append((a, b))
-                    visited[(a, b)] = True
+            i, j, dist = queue.pop(0)
+            
+            if i == n-1 and j == n-1:
+                return dist
+            
+            for a, b in zip(x, y):
+                a = i+a
+                b = j+b
+                if(0<=a<n and 0<=b<n and grid[a][b] == 0):
+                    grid[a][b] = 1
+                    queue.append((a, b, dist+1))
         return -1
