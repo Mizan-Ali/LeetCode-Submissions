e77f9f9d3e8e930b1bcf3764ea99ce3b7facb7b1
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-06 09:35:17 +0530

    Time: 2732 ms (40.64%), Space: 93.3 MB (20.34%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,14 +1,15 @@
 class Solution:
-    def lcs(self, s1, s2):
-        dp = [[0 for _ in range(len(s1)+1)] for _ in range(len(s2)+1)]
-        ans = 0
-        for i in range(1, len(s1)+1):
-            for j in range(1, len(s2)+1):
-                if s1[i-1] == s2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
-                    
+    def lcs(self, i, j, s, dp):
+        if i == j:
+            return 1
+        if i > j:
+            return 0
+        if (i, j) not in dp:
+            if s[i] == s[j]:
+                dp[(i, j)] = 2 + self.lcs(i+1, j-1, s, dp)
+            else:
+                dp[(i, j)] = max(self.lcs(i+1, j, s, dp), self.lcs(i, j-1, s, dp))
+        return dp[(i, j)] 
     def longestPalindromeSubseq(self, s: str) -> int:
-        return self.lcs(s, s[::-1])
+        dp = {}
+        return self.lcs(0, len(s)-1, s, dp)
