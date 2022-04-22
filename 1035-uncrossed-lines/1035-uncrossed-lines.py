e43cfdf6cd4a7cf7e5035c6f88159dd8458625d4
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-22 20:02:20 +0530

    Time: 206 ms (93.81%), Space: 14.2 MB (65.57%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         # Longest common subsequence
-        dp = {}
-        def solve(i, j):
-            if i == len(nums1) or j == len(nums2):
-                return 0
-            if (i, j) not in dp:
-                if nums1[i] == nums2[j]:
-                    dp[(i, j)] = 1 + solve(i+1, j+1)
+        n, m = len(nums1), len(nums2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if nums2[i-1] == nums1[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    dp[(i, j)] = max(solve(i+1, j), solve(i, j+1))
-            return dp[(i, j)]
-
-        i, j = 0, 0
-        return solve(i, j)
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        return dp[-1][-1]
