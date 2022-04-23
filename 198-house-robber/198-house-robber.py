b123570a8442ae7104c39ccaf9a3b98ad7f897d0
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-23 18:03:59 +0530

    Time: 69 ms (5.89%), Space: 13.9 MB (21.87%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,24 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        global ans
         dp = {}
-        ans = 0
-        def solve(idx, amt, dp):
-            global ans
+        def solve(idx):
             if idx >= n:
-                ans = max(ans, amt)
-                return ans
-            
-            if(idx, amt) not in dp:
-                # Rob
-                a = solve(idx+2, amt+nums[idx], dp)
-
-                # Skip
-                b = solve(idx+1, amt, dp)
-            
-                dp[(idx, amt)] = max(a, b)
-            return dp[(idx, amt)]
-            
-        return solve(0, 0, dp)
+                return 0
+            if idx not in dp:
+                dp[idx] = max(nums[idx] + solve(idx+2), solve(idx+1))
+            return dp[idx]
+        return solve(0)
             
