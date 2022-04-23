1591ee8d30cec9ac0ccdd1e80e69e032f5a959e3
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-04-23 13:32:52 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.py
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -13,19 +13,19 @@ class Solution:
             global dp
             if idx == N:
                 if remsum == 0:
-                    return 0
-                return 1
+                    return True
+                return False
             if remsum == 0:
-                return 0
+                return True
             if(idx, remsum) not in dp:
                 if arr[idx] <= remsum:
                     # pick and skip
-                    dp[(idx, remsum)] = solve(idx+1, remsum-arr[idx]) * solve(idx+1, remsum)
+                    dp[(idx, remsum)] = solve(idx+1, remsum-arr[idx]) or solve(idx+1, remsum)
                 else:
-                    return 1
+                    return False
             return dp[(idx, remsum)]
         
-        return not solve(0, sum)
+        return solve(0, sum)
          
         
         
