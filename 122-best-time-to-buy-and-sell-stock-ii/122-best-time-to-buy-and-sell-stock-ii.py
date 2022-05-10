23730d996e53f5e9b6e734bd58a84633cb7f47df
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-05-10 15:28:43 +0530

    Time: 205 ms (5.03%), Space: 46.1 MB (5.04%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        min_so_far = prices[0]
-        
-        for i in range(len(prices)-1):
-            if prices[i+1] - prices[i] > 0:
-                profit += prices[i+1] - prices[i]
-        return profit
+        dp = {}
+        def memo(idx, buy):
+            if idx == len(prices):
+                return 0
+            if (idx, buy) in dp:
+                return dp[(idx, buy)]
+            if buy == 1 :
+                dp[(idx, buy)] = max(-prices[idx] + memo(idx+1, 0),
+                            0 + memo(idx+1, 1))
+            else:
+                dp[(idx, buy)] = max(prices[idx] + memo(idx+1, 1),
+                            0 + memo(idx+1, 0))
+            return dp[(idx, buy)]
+            
+        return memo(0, 1) # 1 means buy || 0 means sell
