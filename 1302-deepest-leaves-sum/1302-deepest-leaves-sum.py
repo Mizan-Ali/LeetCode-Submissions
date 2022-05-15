49176e680c46aa18817d1655da5d130d75cfd2fa
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-05-15 20:40:07 +0530

    Time: 370 ms (21.67%), Space: 17.8 MB (28.19%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -8,14 +8,14 @@ from collections import defaultdict
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         queue = [(root, 0)]
-        levelMap = defaultdict(list)
+        levelMap = defaultdict(int)
         while queue:
             node, level = queue.pop(0)
-            levelMap[level].append(node.val)
+            levelMap[level] += node.val
             if node.left:
                 queue.append((node.left, level+1))
             if node.right:
                 queue.append((node.right, level+1))
         max_level = max(levelMap.keys())
         
-        return sum(levelMap[max_level])
+        return levelMap[max_level]
