2141751007409b86471ba88329e16f26a3cd23b8
Mohd Mizan Ali <64581554+Mizan-Ali@users.noreply.github.com>
2022-05-18 10:18:57 +0530

    Time: 2061 ms (7.51%), Space: 27.9 MB (83.89%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,35 +1,31 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
-        stack = []
-        left = [0 for i in range(n)]
-        right = [0 for i in range(n)]
-        maxArea = 0
-        for i in range(n):
+        left = [0 for _ in heights]
+        stack = [0]
+        
+        for i in range(1, n):
+            while len(stack) != 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
             if len(stack) == 0:
                 left[i] = 0
-                stack.append(i)
             else:
-                while(len(stack)!=0 and heights[stack[-1]] >= heights[i]):
-                    stack.pop()
-                if len(stack) == 0:
-                    left[i] = 0
-                else:
-                    left[i] = stack[-1] + 1
-                stack.append(i)
-        stack = []
-        for i in range(n-1, -1, -1):
+                left[i] = stack[-1] + 1
+            stack.append(i)
+        
+        right = [0 for _ in heights]
+        right[n-1] = n-1
+        stack = [n-1]
+        for i in range(n-2, -1, -1):
+            while len(stack) != 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
             if len(stack) == 0:
                 right[i] = n-1
-                stack.append(i)
             else:
-                while len(stack) != 0 and heights[stack[-1]] >= heights[i]:
-                    stack.pop()
-                if len(stack) == 0:
-                    right[i] = n-1
-                else:
-                    right[i] = stack[-1]-1
-                stack.append(i)
-            maxArea = max(maxArea, (right[i]-left[i]+1)*heights[i])
+                right[i] = stack[-1] - 1
+            stack.append(i)
+        maxArea = 0
         
+        for i in range(n):
+            maxArea = max(maxArea, (right[i]-left[i]+1)*heights[i])
         return maxArea
